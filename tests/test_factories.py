420f28608df98d2c0ea0416def94bcfc959fbbab
James Cooke <jamescooke@users.noreply.github.com>
2016-05-27 15:06:23 +0100

    Add basic tests of UserFactory

---
+++ tests/test_factories.py
@@ -0,0 +1,41 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from factory_djoy.factories import UserFactory
+
+
+class TestAll(TestCase):
+
+    user_model = get_user_model()
+
+    def test_user_default_password(self):
+        """
+        UserFactory sets a usable password as 'password' by default
+
+        Built user is also active
+        """
+        UserFactory()
+
+        self.assertEqual(self.user_model.objects.count(), 1)
+        user = self.user_model.objects.first()
+        self.assertTrue(user.check_password('password'))
+        self.assertTrue(user.is_active)
+
+    def test_user_custom_password(self):
+        """
+        UserFactory can be passed a custom password and sets it
+        """
+        UserFactory(password='cstm_3$!&vBA.,QokHuva')
+
+        user = self.user_model.objects.first()
+        self.assertTrue(user.check_password('cstm_3$!&vBA.,QokHuva'))
+
+    def test_user_factory_create_unusable_pw(self):
+        """
+        UserFactory can create a user with an unusable password
+        """
+        UserFactory(password=None)
+
+        user = self.user_model.objects.first()
+        self.assertFalse(user.has_usable_password())
+        self.assertTrue(user.is_active)
