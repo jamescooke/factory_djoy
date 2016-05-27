ff9c10368dcee1ed1f8ba5a978c99d9eae71b752
James Cooke <jamescooke@users.noreply.github.com>
2016-05-27 18:45:18 +0100

    Install relies on Django and factory boy

--- setup.py
+++ setup.py
@@ -5,12 +5,20 @@ import setuptools
 setuptools.setup(
     name='factory_djoy',
     version='0.1',
+
     description="Wrappers over Factory Boy's Django Factories",
     url='http://github.com/jamescooke/factory_djoy',
     author='James Cooke',
     author_email='github@jamescooke.info',
+
     license='MIT',
+
     packages=setuptools.find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    install_requires=[
+        'Django>=1.6',
+        'factory_boy>=2',
+    ],
+
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
