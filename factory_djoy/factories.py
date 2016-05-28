3a3d6e1845324143819a691f24d9ef90ae5ab116
James Cooke <jamescooke@users.noreply.github.com>
2016-05-28 19:04:34 +0100

    Import full_clean call into UserFactory from prlint

--- factory_djoy/factories.py
+++ factory_djoy/factories.py
@@ -1,17 +1,36 @@
 from django.contrib.auth import get_user_model
-from factory import Sequence, PostGenerationMethodCall
+from factory import LazyFunction, PostGenerationMethodCall, post_generation
 from factory.django import DjangoModelFactory
+from faker.factory import Factory as FakerFactory
+
+
+faker = FakerFactory.create()
 
 
 class UserFactory(DjangoModelFactory):
+    """
+    Factory for default Django User model. Calls full_clean before saving to
+    ensure that generated User instance is valid.
+    """
     class Meta:
         model = get_user_model()
 
-    is_active = True
-    is_staff = False
-    is_superuser = False
-
-    first_name = Sequence(lambda n: 'Joe{}'.format(n))
-    last_name = Sequence(lambda n: 'Bloggs{}'.format(n))
-    email = Sequence(lambda n: 'joe{}@bloggs.com'.format(n))
+    email = LazyFunction(faker.safe_email)
+    first_name = LazyFunction(faker.first_name)
+    last_name = LazyFunction(faker.last_name)
     password = PostGenerationMethodCall('set_password', 'password')
+    username = LazyFunction(lambda: faker.profile()['username'])
+
+    @post_generation
+    def z_full_clean(self, create, extracted, **kwargs):
+        """
+        Assert that created User is "clean" in Django's opinion.
+
+        NOTE: function name is prefixed with 'z_' so that it runs after the
+        'password' post generation function.
+
+        Raises:
+            ValidationError: If there are any invalid fields in the final User
+                object.
+        """
+        self.full_clean()
