01ea936e3a318abcbdcd8694df8e9a04dc4401b5
James Cooke <jamescooke@users.noreply.github.com>
2018-04-20 15:23:54 +0100

    Update setup.py to use about info

--- factory_djoy/__about__.py
+++ factory_djoy/__about__.py
@@ -5,3 +5,4 @@ __author__ = 'James Cooke'
 __copyright__ = '2018, {}'.format(__author__)
 
 __description__ = 'Factories for Django, creating valid model instances every time.'
+__email__ = 'github@jamescooke.info'
