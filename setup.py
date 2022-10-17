363d9f3a9c800ed6661cb5f3a7f2680b0213569f
James Cooke <jamescooke@users.noreply.github.com>
2022-10-17 22:32:39 +0100

    Remove python 3.6 support

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Django>=1.11',
         'factory_boy>=2.11',
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 
     classifiers=[
         'Development Status :: 5 - Production/Stable',
@@ -46,7 +46,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python',
