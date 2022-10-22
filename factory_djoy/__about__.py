1e7172f1511ffc36fc4b3e20b062b9a59f40b90c
James Cooke <jamescooke@users.noreply.github.com>
2022-10-22 22:36:53 +0100

    Bump to v2.2.1

--- factory_djoy/__about__.py
+++ factory_djoy/__about__.py
@@ -1,8 +1,8 @@
 __name__ = 'factory_djoy'
-__version__ = '2.2.0'
+__version__ = '2.2.1'
 
 __author__ = 'James Cooke'
-__copyright__ = '2021, {}'.format(__author__)
+__copyright__ = '2022, {}'.format(__author__)
 
 __description__ = 'Factories for Django, creating valid model instances every time.'
 __email__ = 'github@jamescooke.info'
